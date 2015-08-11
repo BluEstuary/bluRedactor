76538d9e35b57d045810880e4ecd2f4ff409eeb6
bface007 <bface007@gmail.com>
2015-08-11 01:59:09 +0100

    project init

---
+++ README.md
@@ -0,0 +1,5 @@
+# bluRedactor
+### A jQuery HTML5 Wysiwyg Editor
+#### By bface007 (for BluEstuary purposes)
+
+Project is under development
---
+++ assets/js/bluRedactor.js
@@ -0,0 +1,7 @@
+/**
+ * Created by bface007 on 11/08/2015.
+ */
+;(function ($, window, document, undefined) {
+
+    var pluginName = "bluRedactor";
+}(jQuery, window, document));
